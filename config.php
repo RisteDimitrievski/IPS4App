864aa1d1793411166d4898f29534e85919e64b4b
RisteDimitrievski <38003672+RisteDimitrievski@users.noreply.github.com>
2018-04-03 00:26:24 +0200

    New version
    The configuration file which will need to be edited in order to work.

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php 
+$config = array(
+"dbName" => "forum",
+"dbUsername" => "root",
+"dbHost" => "localhost",
+"dbPassword" => "1234".
+"charset" => "UTF8",
+"Site_Url" => "http://localhost/forums/",
+"table" => "core_members");
+/*
+If you don't know what you're doing for charset, then dont change it.
+Because, some charsets are not safe for using in production servers.
+
+NOTE: If your IPS Forum installation have custom prefix, you should change the core_members with the prefix entered in the forums database.
+And the database name must be exactly like in your forums.
+*/
+
+$dsn = "mysql://host=".$config["dbHost"].";dbname=".$config["dbName"];
+if(empty($config["table"] or $config["Site_Url"] or $config["dbName"] or $config["dbUsername"] or $config["dbPassword"] or $config["dbHost"] or $config["charset"]))
+{
+	throw new Exception("Enter the database informations in config.php");
+}
+
+$options = array(
+PDO::MYSQL_ATTR_INT_COMMAND => "SET NAMES ".$config["charset"]);
+
+$db = new PDO($dsn,$config["dbUsername"],$config["dbPassword"]);
+if(!$db)
+{
+	die($db->error);
+}
+
+
+
+?>
+
